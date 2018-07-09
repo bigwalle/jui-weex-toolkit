40932cc21e17f49060f461be08798481ef680460
welcome112s <welcome112s>
2018-07-09 17:45:43 +0800

    新建了 jui-weex-toolkit 项目

---
+++ index.js
@@ -0,0 +1,9 @@
+const TemplateRelease = require('./template-release');
+
+module.exports = {
+    TemplateRelease,
+    constants: {
+        templateReleaseUrl: "https://api.github.com/repos/welcome112s/jui-weex-template/releases",
+        cacheDirName: 'jui-weex'
+    }
+}
